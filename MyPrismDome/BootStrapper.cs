cbc6a319e3e8ed3a3bd7be5145478488aecf540f
Administrator <Administrator@USERCHI-K3PCVHJ>
2018-11-05 14:56:59 +0800

    添加prism框架的demo

--- MyPrismDome/BootStrapper.cs
+++ MyPrismDome/BootStrapper.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Windows;
 using Microsoft.Practices.Prism.UnityExtensions;
 using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Prism.Modularity;
 
 namespace MyPrismDome
 {
@@ -17,17 +18,25 @@ namespace MyPrismDome
         /// <returns></returns>
         protected override DependencyObject CreateShell()
         {
-            return ServiceLocator.Current.GetInstance<Shell>();
+            return new Shell();
         }
 
         protected override void InitializeShell()
         {
             base.InitializeShell();
+
+            App.Current.MainWindow = (Window)this.Shell;
+
+            App.Current.MainWindow.Show();
         }
 
         protected override void ConfigureModuleCatalog()
         {
             base.ConfigureModuleCatalog();
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+
+            moduleCatalog.AddModule(typeof(MyPrismDomeView.NavigationModule));
+
         }
     }
 }
---
+++ MyPrismDomeView/NavigationModule.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Regions;
+
+namespace MyPrismDomeView
+{
+    public class NavigationModule : IModule
+    {
+        private IRegionManager _regionmodule;
+        public NavigationModule(IRegionManager module)
+        {
+            this._regionmodule = module;
+        }
+        public void Initialize()
+        {
+            _regionmodule.RegisterViewWithRegion("navigationCotrol", typeof(View.Navigation));
+        }
+    }
+}
